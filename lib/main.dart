8bda7e33ca29b87cdbb23a350fe9d70faf82e5a9
Vishakha <40317353+Vishakha2697@users.noreply.github.com>
2019-07-11 04:56:44 -0700

    Ques ans pattern

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,72 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
         appBar: AppBar(
           title: Text('Quizzler'),
         ),
+        body: SafeArea(
+            child: Padding(
+    padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: Quiz(),
+        ),
+      ),
       ),
     );
   }
 }
   
 
+class Quiz extends StatefulWidget {
+  @override
+  _QuizState createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+            child: Center(
+              child: Text('Questions',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+        ),
+        Expanded(
+            child: FlatButton(
+                onPressed: null,
+                child: Text('Option A',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+            ),
+        ),
+        Expanded(
+          child: FlatButton(
+              onPressed: null,
+              child: Text('Option B',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+              onPressed: null,
+              child: Text('Option C',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+          ),
+        ),
+      ],
+    );
+  }
+}
