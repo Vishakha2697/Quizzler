558817e91a44b475010fdaa4e108ce7727e75148
Vishakha <40317353+Vishakha2697@users.noreply.github.com>
2019-07-11 10:48:38 -0700

    Questions Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'question.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,9 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
-        appBar: AppBar(
-          title: Text('Quizzler'),
-        ),
+//        appBar: AppBar(
+//          title: Text('Quizzler'),
+//        ),
         body: SafeArea(
             child: Padding(
     padding: EdgeInsets.symmetric(horizontal: 10.0),
@@ -34,46 +35,81 @@ class _QuizState extends State<Quiz> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
             child: Center(
-              child: Text('Questions',
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
                 style: TextStyle(
+                  fontSize: 25.0,
                   color: Colors.white,
                 ),
               ),
             ),
+          ),
         ),
         Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
             child: FlatButton(
-                onPressed: null,
-                child: Text('Option A',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+              textColor: Colors.white,
+              color: Colors.amber,
+              child: Text(
+                'Option A',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
                 ),
+              ),
+              onPressed: () {
+                //The user picked true.
+              },
             ),
-        ),
-        Expanded(
-          child: FlatButton(
-              onPressed: null,
-              child: Text('Option B',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
           ),
         ),
         Expanded(
-          child: FlatButton(
-              onPressed: null,
-              child: Text('Option C',
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.pink,
+              child: Text(
+                'Option B',
                 style: TextStyle(
+                  fontSize: 20.0,
                   color: Colors.white,
                 ),
               ),
+              onPressed: () {
+                //The user picked false.
+              },
+            ),
           ),
         ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.blue,
+              child: Text(
+                'Option C',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+              },
+            ),
+          ),
+        ),
+        //TODO: Add a Row here as your score keeper
       ],
     );
   }
